cb3e43899a8146a0a0b40440d969913b8814739f
iamanjanap04-tech <iamanjanap04@gmail.com>
2026-03-25 22:41:30 +0530

    fix duplicate db declaration

--- routes/products.js
+++ routes/products.js
@@ -3,7 +3,6 @@ const router = express.Router();
 
 const db = require('../config/db');
 
-const db = require("../config/db");
 
 const { auth, adminOnly } = require('../middleware/auth');
 const multer = require('multer');
